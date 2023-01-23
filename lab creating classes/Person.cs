7d7c2235e8c5fe5597bc9e72f85c73ff78fa4f4b
Sean <sean.lam9282@gmail.com>
2023-01-22 22:44:09 -0700

    1st commit - Question 3g not completed

---
+++ lab creating classes/Person.cs
@@ -0,0 +1,75 @@
+﻿using lab_creating_classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_creating_classes
+{
+    internal class Person
+    {
+        // Q1
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private string favouriteColour;
+        private int age;
+        private bool isWorking;
+
+        public Person(int personId, string firstName, string lastName, string favouriteColour, int age, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.favouriteColour = favouriteColour;
+            this.age = age;
+            this.isWorking = isWorking;
+        }
+
+        public void DisplayPersonInfo()
+        {
+            string formatted = string.Format("{0}: {1} {2}'s favorite color is {3}", personId, firstName, lastName, favouriteColour);
+            Console.WriteLine(formatted);
+        }
+
+        public string ChangeFavoriteColour
+        {
+            get 
+            {
+                favouriteColour = "White";
+                return favouriteColour;
+            }
+        }
+
+        public void GetAgeInTenYears()
+        {
+            int newAge = age + 10;
+
+            string formatted = string.Format("{0} {1}'s Age in 10 years is: {2}", firstName, lastName, newAge);
+            Console.WriteLine(formatted);
+        }
+
+        public override string ToString()
+        {
+            //Console.WriteLine("PersonId: {0}\nFirstName: {1}\nLastName: {2}\nFavouriteColour: {3}\nAge: {4}\nIsWorking: {5}",personId, firstName, lastName, favouriteColour, age, isWorking);
+            return $"PersonId: {personId}\nFirstName: {firstName}\nLastName: {lastName}\nFavouriteColour: {favouriteColour}\nAge: {age}\nIsWorking: {isWorking}";
+        }
+
+        public double GetAge
+        {
+            get
+            {
+                return age;
+            }
+        }
+
+        public string GetName
+        {
+            get
+            {
+                return firstName;
+            }
+        }
+    }
+}
---
+++ lab creating classes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_creating_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Q3a
+            Person person1 = new Person(1, "Ian", "Brooks", "Red", 30, true);
+            Person person2 = new Person(2, "Gina", "James", "Green", 18, false);
+            Person person3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+            Person person4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+            // Q3b
+            person2.DisplayPersonInfo();
+
+            // Q3c
+            Console.WriteLine(person3.ToString());
+
+            // Q3d
+            string newColor = person1.ChangeFavoriteColour;
+            person1.DisplayPersonInfo();
+
+            // Q3e
+            person4.GetAgeInTenYears();
+            
+            // Q3g
+            Person[] people = new Person[] {person1, person2, person3, person4};
+
+            double averageAge = (people[0].GetAge + people[1].GetAge + people[2].GetAge + people[3].GetAge) / 4;
+            //double averageAge = people[0..3].GetAge / 4;
+            Console.WriteLine("Average age is: " + averageAge);
+
+            var nameAgeTable = new Dictionary<double, string>()
+            {
+                {people[0].GetAge, people[0].GetName},
+                {people[1].GetAge, people[1].GetName},
+                {people[2].GetAge, people[2].GetName},
+                {people[3].GetAge, people[3].GetName},
+            };
+            Console.WriteLine("The youngest person is: " + nameAgeTable[new[] {people[0].GetAge, people[1].GetAge, people[2].GetAge, people[3].GetAge}.Min()]);
+            Console.WriteLine("The oldest person is: " + nameAgeTable[new[] {people[0].GetAge, people[1].GetAge, people[2].GetAge, people[3].GetAge}.Max()]);
+        }
+    }
+}
